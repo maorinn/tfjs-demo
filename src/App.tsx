cd0b2da02de4163b5f73a030e00438cf75165f90
maoring <maoring21@gmail.com>
2022-11-03 09:29:28 +0800

    feat:初步集成手势检测模型

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,24 @@ import { loadGraphModel } from "@tensorflow/tfjs-converter";
 import "./style/App.css";
 import { renderBoxes } from "./utils/renderBox";
 import { Webcam } from "./utils/webcam";
+import * as handdetection from "@tensorflow-models/hand-pose-detection";
+import * as handPoseDetection from "@tensorflow-models/hand-pose-detection";
+let detector: {
+  estimateHands: (arg0: any, arg1: { flipHorizontal: boolean }) => any;
+  dispose: () => void;
+} | null = null;
+let rafId;
+// 是否开始加载模型
+let isModelReady = false;
+// TODO fix: 目标检测和手部检测 的预测结果会出现 两个 canvas 重叠的情况
 function App() {
   const videoRef: any = useRef();
   const canvasRef: any = useRef();
   const [facingMode, setFacingMode] = useState("environment");
-  const threshold = 0.70;
+  const threshold = 0.3;
   let model = null;
-  const webcam = new Webcam();
+
+  let webcam: any;
   const loadModel = async () => {
     // const model = await loadGraphModel(
     //   "https://raw.githubusercontent.com/Hyuto/yolov5-tfjs/master/public/yolov5n_web_model/model.json"
@@ -36,14 +47,13 @@ function App() {
             .expandDims(0);
         });
         return input;
-      }catch(e){
+      } catch (e) {
         console.log(e);
         return null;
       }
     } else {
       return null;
     }
-   
   };
   const detectFrame = async (model: any) => {
     tf.engine().startScope();
@@ -86,11 +96,65 @@ function App() {
     }
     webcam.open(videoRef, () => {}, newFacingMode);
   };
+  /**
+   * 初始化 手部检测模型
+   * @returns
+   */
+  const createDetector = async () => {
+    const model = handPoseDetection.SupportedModels.MediaPipeHands;
+    return handdetection.createDetector(model, {
+      runtime: "mediapipe",
+      modelType: "lite",
+      maxHands: 2,
+      solutionPath: "https://cdn.jsdelivr.net/npm/@mediapipe/hands",
+    });
+  };
+  const renderResult = async () => {
+    if (webcam.video.readyState < 2) {
+      await new Promise((resolve) => {
+        webcam.video.onloadeddata = () => {
+          resolve(webcam);
+        };
+      });
+    }
+    let hands = null;
+    // Detector can be null if initialization failed (for example when loading
+    // from a URL that does not exist).
+    if (detector != null) {
+      // Detectors can throw errors, for example when using custom URLs that
+      // contain a model that doesn't provide the expected output.
+      try {
+        hands = await detector.estimateHands(videoRef.current, {
+          flipHorizontal: false,
+        });
+      } catch (error) {
+        detector.dispose();
+        detector = null;
+        alert(error);
+      }
+    }
+    // The null check makes sure the UI is not in the middle of changing to a
+    // different model. If during model change, the result is from an old model,
+    // which shouldn't be rendered.
+    if (hands && hands.length > 0) {
+      webcam.drawResults(hands);
+    }
+  };
+  const renderPrediction = async () => {
+    if (!detector && !isModelReady) {
+      isModelReady = true
+      detector = await createDetector()
+    }
+    await renderResult();
+    rafId = requestAnimationFrame(renderPrediction);
+  };
 
   useEffect(() => {
+    webcam = new Webcam(videoRef.current, canvasRef.current);
     loadModel().then(async (_model) => {
       // 预热
       const dummyInput = tf.ones(_model.inputs[0].shape as any);
+      // @ts-ignore
       await _model.executeAsync(dummyInput).then((warmupResult: any) => {
         // 清除预热结果
         tf.dispose(warmupResult);
@@ -99,14 +163,16 @@ function App() {
         webcam.open(videoRef, () => detectFrame(_model), facingMode);
       });
     });
+    // // 加载手部检测模型
+    renderPrediction();
   }, []);
   return (
     <div className="App">
       <h2>胰岛素场景医疗AI识别demo YOLOv5 & Tensorflow.js</h2>
       <p>Currently running model : YOLOv5</p>
       <div className="content">
-        <video autoPlay playsInline muted ref={videoRef} />
-        <canvas width={640} height={640} ref={canvasRef} />
+        <video id="video" autoPlay playsInline muted ref={videoRef} />
+        <canvas id="canvas" width={640} height={480} ref={canvasRef} />
         <button onClick={switchCamera}>切换摄像头</button>
       </div>
     </div>
--- src/utils/webcam.ts
+++ src/utils/webcam.ts
@@ -1,13 +1,23 @@
+import * as params from "./params";
+
 /**
  * Class to handle webcam
  */
 export class Webcam {
+  canvas: any;
+  video: any;
+  ctx: any;
+  constructor(video: any, canvas: any) {
+    this.video =  document.getElementById('video');;
+    this.canvas =  document.getElementById('canvas');
+    this.ctx = this.canvas.getContext("2d");
+  }
   /**
    * Open webcam and stream it through video tag.
    * @param {React.MutableRefObject} videoRef video tag reference
    * @param {function} onLoaded callback function to be called when webcam is open
    */
-  open = (videoRef:any, onLoaded:any,facingMode:string) => {
+  open = (videoRef: any, onLoaded: any, facingMode: string) => {
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
       navigator.mediaDevices
         .getUserMedia({
@@ -31,7 +41,7 @@ export class Webcam {
    * Close opened webcam.
    * @param {React.MutableRefObject} videoRef video tag reference
    */
-  close = (videoRef:any) => {
+  close = (videoRef: any) => {
     if (videoRef.current.srcObject) {
       videoRef.current.srcObject = null;
       // @ts-ignore
@@ -40,4 +50,91 @@ export class Webcam {
       });
     } else alert("Please open Webcam first!");
   };
+  drawCtx() {
+    this.ctx.drawImage(
+        this.video, 0, 0, this.video.videoWidth, this.video.videoHeight);
+  }
+  /**
+   * 绘制手部关键点识别结果
+   * @param hands
+   */
+  drawResults(hands: any) {
+    // Sort by right to left hands.
+    hands.sort((hand1: any, hand2: any) => {
+      if (hand1.handedness < hand2.handedness) return 1;
+      if (hand1.handedness > hand2.handedness) return -1;
+      return 0;
+    });
+
+    // Pad hands to clear empty scatter GL plots.
+    while (hands.length < 2) hands.push({});
+
+    for (let i = 0; i < hands.length; ++i) {
+      // Third hand and onwards scatterGL context is set to null since we
+      // don't render them.
+      const ctxt = null;
+      this.drawResult(hands[i], ctxt);
+    }
+  }
+  /**
+   * 绘制手部关键点识别结果
+   * @param hand A hand with keypoints to render.
+   * @param ctxt Scatter GL context to render 3D keypoints to.
+   */
+  drawResult(hand: any, ctxt: any) {
+    if (hand.keypoints != null) {
+      this.drawKeypoints(hand.keypoints, hand.handedness);
+    }
+    // Don't render 3D hands after first two.
+    if (ctxt == null) {
+      return;
+    }
+  }
+  /**
+   * 绘制手部关键点
+   * @param keypoints
+   * @param handedness
+   */
+  drawKeypoints(keypoints: any, handedness: any) {
+    const keypointsArray = keypoints;
+    this.ctx.fillStyle = handedness === "Left" ? "Red" : "Blue";
+    this.ctx.strokeStyle = "White";
+    this.ctx.lineWidth = params.DEFAULT_LINE_WIDTH;
+
+    for (let i = 0; i < keypointsArray.length; i++) {
+      const y = keypointsArray[i].x;
+      const x = keypointsArray[i].y;
+      this.drawPoint(x -2, y - 2, 3);
+    }
+
+    const fingers = Object.keys(params.fingerLookupIndices);
+    for (let i = 0; i < fingers.length; i++) {
+      const finger = fingers[i];
+      // @ts-ignore
+      const points = params.fingerLookupIndices[finger].map(
+        (idx:any) => keypoints[idx]
+      );
+      this.drawPath(points, false);
+    }
+  }
+
+  drawPath(points: any, closePath: any) {
+    const region = new Path2D();
+    region.moveTo(points[0].x, points[0].y);
+    for (let i = 1; i < points.length; i++) {
+      const point = points[i];
+      region.lineTo(point.x, point.y);
+    }
+
+    if (closePath) {
+      region.closePath();
+    }
+    this.ctx.stroke(region);
+  }
+
+  drawPoint(y: number, x: number, r: number) {
+    this.ctx.beginPath();
+    this.ctx.arc(x, y, r, 0, 2 * Math.PI);
+    this.ctx.fill();
+  }
 }
